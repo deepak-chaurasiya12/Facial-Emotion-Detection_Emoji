7f2f3d8f29cae037bd77bcf9c70a534ed929f4b1
deepak-chaurasiya12 <dchaurasiya277@gmail.com>
2022-02-17 12:49:56 +0530

    second commit

--- emoji.py
+++ emoji.py
@@ -21,8 +21,6 @@ from keras.preprocessing.image import ImageDataGenerator
 
 emotion_model = Sequential()
 
-
-
 emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
 emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
 emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
